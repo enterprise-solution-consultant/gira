30246b8492b21e5d84d1579ae1ed307b5e958e72
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2022-09-18 15:45:52 +0700

    feat: add BaseEntity with Jpa Auditor, CJB-3

---
+++ src/main/java/cybersoft/javabackend/java18/gira/common/model/BaseEntity.java
@@ -0,0 +1,70 @@
+package cybersoft.javabackend.java18.gira.common.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import cybersoft.javabackend.java18.gira.common.util.DateTimeUtils;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import lombok.experimental.UtilityClass;
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity implements Serializable {
+    @Id
+    @Type(type = "uuid-char")
+    @GeneratedValue
+    @Column(name = Columns.ID)
+    protected UUID id;
+
+    @Version
+    @Column(name = Columns.VERSION)
+    protected int version;
+
+    @CreatedBy
+    @Column(name = Columns.CREATED_BY)
+    protected String createdBy;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateTimeUtils.DATETIME_FORMAT)
+    @DateTimeFormat(pattern = DateTimeUtils.DATETIME_FORMAT)
+    @CreatedDate
+    @Column(name = Columns.CREATED_AT)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @Column(name = Columns.LAST_MODIFIED_BY)
+    protected String lastModifiedBy;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateTimeUtils.DATETIME_FORMAT)
+    @DateTimeFormat(pattern = DateTimeUtils.DATETIME_FORMAT)
+    @LastModifiedDate
+    @Column(name = Columns.LAST_MODIFIED_AT)
+    protected LocalDateTime lastModifiedAt;
+
+    // inner class
+    @UtilityClass
+    static class Columns {
+        static final String ID = "ID";
+        static final String VERSION = "VERSION";
+        static final String CREATED_BY = "CREATED_BY";
+        static final String CREATED_AT = "CREATED_AT";
+        static final String LAST_MODIFIED_BY = "LAST_MODIFIED_BY";
+        static final String LAST_MODIFIED_AT = "LAST_MODIFIED_AT";
+    }
+}
---
+++ src/main/java/cybersoft/javabackend/java18/gira/common/util/DateTimeUtils.java
@@ -0,0 +1,8 @@
+package cybersoft.javabackend.java18.gira.common.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class DateTimeUtils {
+    public static final String DATETIME_FORMAT = "dd-MM-yyyy HH:mm:ss";
+}
